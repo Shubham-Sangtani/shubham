42368bcb1651f13c0e6311a54eb743e1d921b4c1
Shubham-Sangtani <110885024+Shubham-Sangtani@users.noreply.github.com>
2022-09-03 13:43:56 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+// java program to reverse a word
+
+import java.io.*;
+import java.util.Scanner;
+
+class ReverseString {
+	public static void main (String[] args) {
+
+		String str= "HelloWorld", nstr="";
+		char ch;
+
+	System.out.print("Original word: ");
+	System.out.println("HelloWorld"); //Example word
+
+	for (int i=0; i<str.length(); i++)
+	{
+		ch= str.charAt(i); //extracts each character
+		nstr= ch+nstr; //adds each character in front of the existing string
+	}
+	System.out.println("Reversed word: "+ nstr);
+	}
+}
+
